048c204eecbf34bc1c779d6146cb7b45039efde3
mogoo7zn <znwangking@hotmail.com>
2025-05-14 22:25:23 +0800

    add AKI

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -1,53 +1,16 @@
 #include "napi/native_api.h"
+#include <aki/jsbind.h>
+#include <string> 
 
-static napi_value Add(napi_env env, napi_callback_info info)
-{
-    size_t argc = 2;
-    napi_value args[2] = {nullptr};
-
-    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
-
-    napi_valuetype valuetype0;
-    napi_typeof(env, args[0], &valuetype0);
-
-    napi_valuetype valuetype1;
-    napi_typeof(env, args[1], &valuetype1);
-
-    double value0;
-    napi_get_value_double(env, args[0], &value0);
-
-    double value1;
-    napi_get_value_double(env, args[1], &value1);
-
-    napi_value sum;
-    napi_create_double(env, value0 + value1, &sum);
-
-    return sum;
-
-}
-
-EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports)
-{
-    napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
-    };
-    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
-    return exports;
-}
-EXTERN_C_END
-
-static napi_module demoModule = {
-    .nm_version = 1,
-    .nm_flags = 0,
-    .nm_filename = nullptr,
-    .nm_register_func = Init,
-    .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
-};
-
-extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
-{
-    napi_module_register(&demoModule);
+// 1、用户自定义业务 
+std::string SayHello(std::string msg){  return msg + " too.";}  
+ 
+// 2、导出业务接口 
+// Step 1 注册 AKI 插件 
+JSBIND_ADDON(entry) // 注册 AKI 插件名: 即为编译*.so名称，规则与Node-API一致 
+ 
+// Step 2 注册 FFI 特性 
+JSBIND_GLOBAL() 
+{ 
+  JSBIND_FUNCTION(SayHello); 
 }
